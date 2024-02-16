3af97892a3edecf74665cc3f6d66f4ea99d1d2d2
ruah0807 <kimhk0315@gmail.com>
2024-02-16 12:50:27 +0900

    first project commit

---
+++ src/main/java/org/ohgiraffers/ruruproject1/Ruruproject1Application.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.ruruproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ruruproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ruruproject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/ohgiraffers/ruruproject1/Ruruproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.ruruproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ruruproject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
